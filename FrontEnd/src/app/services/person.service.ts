6076c372c1d3fcad452472a09f28d3467de64b50
Embm5 <emmanuel_bolivar82212@elpoli.edu.co>
2025-05-27 03:23:46 -0500

    Add funcionalities
    adeed vercel

--- FrontEnd/src/app/services/person.service.ts
+++ FrontEnd/src/app/services/person.service.ts
@@ -5,37 +5,41 @@ import { Person } from '../interfaces/person';
 import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PersonService {
-
-  private myAppUrl: string
-  private myApiUrl: string
+  private myAppUrl: string;
+  private myApiUrl: string;
   constructor(private http: HttpClient) {
-    this.myAppUrl = environment.appUrl
-    this.myApiUrl = 'api/person'
+    this.myAppUrl = 'https://pro-paralela-l88e.vercel.app/';
+    this.myApiUrl = 'api/person';
   }
   login(user: any): Observable<any> {
-    return this.http.post<any>(`${this.myAppUrl}${this.myApiUrl}/login`, user)
+    return this.http.post<any>(`${this.myAppUrl}${this.myApiUrl}/login`, user);
   }
 
   getPerson(nit: string): Observable<Person> {
-    return this.http.get<Person>(`${this.myAppUrl}${this.myApiUrl}/${nit}`)
+    return this.http.get<Person>(`${this.myAppUrl}${this.myApiUrl}/${nit}`);
   }
 
   deletePerson(nit: string): Observable<any> {
-    return this.http.delete<any>(`${this.myAppUrl}${this.myApiUrl}/${nit}`)
+    return this.http.delete<any>(`${this.myAppUrl}${this.myApiUrl}/${nit}`);
   }
 
   createPerson(person: Person): Observable<any> {
-    return this.http.post<any>(`${this.myAppUrl}${this.myApiUrl}`, person)
+    return this.http.post<any>(`${this.myAppUrl}${this.myApiUrl}`, person);
   }
 
   updatePerson(nit: string, person: Person): Observable<Person> {
-    return this.http.put<Person>(`${this.myAppUrl}${this.myApiUrl}/${nit}`, person)
+    return this.http.put<Person>(
+      `${this.myAppUrl}${this.myApiUrl}/${nit}`,
+      person
+    );
   }
 
   getPersonXmlReport(): Observable<string> {
-    return this.http.get(`${this.myAppUrl}${this.myApiUrl}/report/xml`, { responseType: 'text' })
+    return this.http.get(`${this.myAppUrl}${this.myApiUrl}/report/xml`, {
+      responseType: 'text',
+    });
   }
 }
